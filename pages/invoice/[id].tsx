0f9e4516e9cfeb510ae6e947e3709fce0d2f9bea
aleksFedotov <aleksfedotov.dev@gmail.com>
2022-06-14 08:36:20 +0300

    bug fixed

--- pages/invoice/[id].tsx
+++ pages/invoice/[id].tsx
@@ -16,11 +16,14 @@ import { useRouter } from 'next/router';
 import IconArrowLeft from '../../public/assets/icon-arrow-left.svg';
 import InvoiceForm from '../../components/shered/form/InvoiceForm';
 import prisma from '../../client';
+import useWindowWidth from '../../hooks/useWindowWidth';
+import ViewButtons from '../../components/ivoice-view/view-buttons/ViewButtons';
 
 const InvoceView: NextPage<{ invoiceData: IInvoice }> = ({ invoiceData }) => {
   const modalIsOpened = useAppSelector(selectDeleteModal);
   const isFormModalOpened = useAppSelector(selectformModal);
-  const { invoices } = useAppSelector(selectDemo);
+  const { invoices, isDemoMode } = useAppSelector(selectDemo);
+  const windowWidth = useWindowWidth();
   const router = useRouter();
 
   if (!invoiceData) {
@@ -48,6 +51,12 @@ const InvoceView: NextPage<{ invoiceData: IInvoice }> = ({ invoiceData }) => {
       </Button>
       <InvoiceViewHeader data={invoiceData} />
       <InvoiceViewContent data={invoiceData} />
+      {windowWidth! < 700 && (
+        <ViewButtons
+          isMobile={true}
+          invoiceId={isDemoMode ? invoiceData.id : invoiceData.id_db}
+        />
+      )}
       <AnimatePresence>
         {modalIsOpened && (
           <Modal type="delete" key="modal">
