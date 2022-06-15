6d2ee29d69a913edcc3ffd693debae80696e9955
aleksFedotov <aleksfedotov.dev@gmail.com>
2022-06-15 07:04:29 +0300

    auth comments

--- components/auth/Auth.tsx
+++ components/auth/Auth.tsx
@@ -19,20 +19,25 @@ type AuthInputs = {
   passwordConfirmation?: string;
 };
 
+// Schema for validation in react hook form for login
 const schemaLogin = yup.object().shape({
+  // Should be correct email
   email: yup.string().email().required('Invalid email'),
+  // Password field should not be empty and should have length at least 6
   password: yup
     .string()
     .min(6, 'Should be at least 6 characters long')
     .required("Can't be epmty"),
 });
 
+// Schema for validation in react hook form for sing up
 const schemaSignin = yup.object().shape({
   email: yup.string().email().required('Invalid email'),
   password: yup
     .string()
     .min(6, 'Should be at least 6 characters long')
     .required("Can't be epmty"),
+  // Repeated password should not be empty and should be the same as pasword
   passwordConfirmation: yup
     .string()
     .oneOf([yup.ref('password'), null], 'Password must match')
@@ -40,16 +45,24 @@ const schemaSignin = yup.object().shape({
 });
 
 const Auth: React.FC = () => {
+  // States
   const [isLoginMode, setIsLoginMode] = useState<boolean>(false);
+
+  // Hooks
   const { isLoading, error: httpError, sendRequest } = useHttp();
   const dispatch = useAppDispatch();
   const router = useRouter();
-
   const { handleSubmit, control } = useForm<AuthInputs>({
+    // depends on auth mode we have diffent validation schema
     resolver: yupResolver(isLoginMode ? schemaLogin : schemaSignin),
   });
 
+  // Submit Handler
   const onSubmit: SubmitHandler<AuthInputs> = async (data) => {
+    // First create expiration date and then send request login request to
+    // server with senRequest function, if everything is ok we dispacth logn action
+    //  and demoLogout action. So we have all user data stored in global state and
+    //  app do not render demo data. After that we redirect app to home paage
     try {
       const tokenExpirationDate = new Date(
         new Date().getTime() + 1000 * 60 * 60
@@ -81,8 +94,10 @@ const Auth: React.FC = () => {
 
   return (
     <AuthWrapper>
+      {/* If useHttp returns error display here */}
       {httpError && <ErorrMessage>{httpError}</ErorrMessage>}
       <AuthForm onSubmit={handleSubmit(onSubmit)}>
+        {/* While request in snedRequest is pending we have loading spinner */}
         {isLoading && <LoadingSpinner asOverlay text="Submitting..." />}
         <h1>{isLoginMode ? 'Login' : 'Sign Up'}</h1>
         <Controller
@@ -117,6 +132,7 @@ const Auth: React.FC = () => {
             />
           )}
         />
+        {/* If user try to sing up then it has to repeated password for his new account */}
         {!isLoginMode && (
           <Controller
             control={control}
